d7ece6e632e46a9c3a9740fd87f5c3f2a0cf20d0
daro <daro@tsr>
2024-04-23 15:42:35 +0300

    bugfix in create_project.tcl, new targests in Makefile

--- hw/system/snitch_isolde/create_project.tcl
+++ hw/system/snitch_isolde/create_project.tcl
@@ -1,6 +1,6 @@
 ################################################################
 # configure board
-set board_suffix zcu102
+set board_suffix zcu104
 source ../../../board/xilinix_${board_suffix}.cfg
 ################################################################
 create_project $project  ./vivado/$project  -part $part  -force
@@ -8,7 +8,7 @@ source generated/sources.tcl
 set_property top snitch_cluster_wrapper [current_fileset]
 update_compile_order -fileset sources_1
 if {[regexp -nocase {.*board_part.*} [list_property [current_project]]]} {
-  set_property board_part xilinx.com:zcu102:part0:3.4 [current_project]
+  set_property board_part $board [current_project]
 } else {
-  set_property board xilinx.com:zcu102:part0:3.4 [current_project]
+  set_property board $board [current_project]
 }
