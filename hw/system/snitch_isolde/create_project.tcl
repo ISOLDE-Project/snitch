ac5a75c498412a0d031f552bf0cff23dcee0fa46
daro <daro@tsr>
2024-04-23 20:55:24 +0300

    failed attempt to generate VivadoIP

--- hw/system/snitch_isolde/create_project.tcl
+++ hw/system/snitch_isolde/create_project.tcl
@@ -5,7 +5,7 @@ source ../../../board/xilinix_${board_suffix}.cfg
 ################################################################
 create_project $project  ./vivado/$project  -part $part  -force
 source generated/sources.tcl
-set_property top snitch_cluster_wrapper [current_fileset]
+set_property top top_isolde_ip [current_fileset]
 update_compile_order -fileset sources_1
 if {[regexp -nocase {.*board_part.*} [list_property [current_project]]]} {
   set_property board_part $board [current_project]
