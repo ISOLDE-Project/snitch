7f38809c362a2e6c27f2e2a7b9730886a5b97060
darotsr <daro@tsr>
2024-07-23 17:25:15 +0300

    faster make bin/snitch_cluster.vlt

--- hw/ip/test/src/common_lib.cc
+++ hw/ip/test/src/common_lib.cc
@@ -15,10 +15,10 @@ extern const uint8_t tb_bootrom_start;
 extern const uint8_t tb_bootrom_end;
 }
 
-asm(".global tb_bootrom_start \n"
-    ".global tb_bootrom_end \n"
-    "tb_bootrom_start: .incbin \"test/bootrom.bin\" \n"
-    "tb_bootrom_end: \n");
+// asm(".global tb_bootrom_start \n"
+//     ".global tb_bootrom_end \n"
+//     "tb_bootrom_start: .incbin \"test/bootrom.bin\" \n"
+//     "tb_bootrom_end: \n");
 
 // The global memory all memory ports write into.
 GlobalMemory MEM;
